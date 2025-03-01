edc9edf0998bc0d4693302fc1a69ace16d37abab
nikneym <halildrk@gmail.com>
2025-03-01 11:32:58 +0300

    chore: rename main.zig to bench.zig

--- build.zig
+++ build.zig
@@ -34,7 +34,7 @@ pub fn build(b: *std.Build) void {
         // only contains e.g. external object files, you can make this `null`.
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/bench.zig"),
         .target = target,
         .optimize = optimize,
     });
